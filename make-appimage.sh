231639136d8e81b7cd691cf363cc6828b48153cc
Link4Electronics <link4electronics@gmail.com>
2026-03-11 00:27:47 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,7 +10,7 @@ export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=https://raw.githubusercontent.com/odamex/odamex/refs/heads/stable/media/icon_odalaunch_512.png
 export DESKTOP=/usr/share/applications/net.odamex.Odamex.Launcher.desktop
-export DEPLOY_OPENGL=1
+export DEPLOY_QT=1
 
 # Deploy dependencies
 quick-sharun usr/bin/odalaunch usr/bin/odamex usr/bin/odasrv #/usr/share/odamex/*
